38eca479c18fd1a6aa8da4679ca915e97301b59c
Matt Selph <matt.selph@gmail.com>
2019-11-15 09:07:35 -0500

    Completed pointers lesson

---
+++ lgwt/pointers/wallet.go
@@ -0,0 +1,39 @@
+package pointers
+
+import (
+	"fmt"
+	"errors"
+)
+
+type Bitcoin int
+
+type Stringer interface {
+	String()	string
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+	w.balance -= amount
+	return nil
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
