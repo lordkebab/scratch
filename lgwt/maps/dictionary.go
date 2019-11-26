f4dc4427729a36bd4e6e53ea31555f8b5e58f6a1
Matt Selph <matt.selph@gmail.com>
2019-11-26 15:09:29 -0500

    Finished the maps lesson

---
+++ lgwt/maps/dictionary.go
@@ -0,0 +1,56 @@
+package maps
+
+import (
+	"errors"
+)
+
+type Dictionary map[string]string
+
+var (
+	ErrNotFound = errors.New("could not find the word you were looking for")
+	ErrWordExists = errors.New("word already exists in dictionary")
+	ErrWordDoesNotExist = errors.New("word does not exist")
+) 
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+	
+	return nil
+}
+
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
+}
